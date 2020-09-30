12f34bbc17eb30b7da100cc2f94aca8b2f13340b
DivadNojnarg <dgranjon@ymail.com>
2020-09-30 14:51:21 +0200

    add hints

--- inst/bulma-custom/bulma-modal.js
+++ inst/bulma-custom/bulma-modal.js
@@ -2,6 +2,7 @@ $(function() {
   Shiny.addCustomMessageHandler('toggle-bulma-modal', function(message) {
     var modalId = '#' + ...;
     var modal = Bulma(...).modal();
+    // a shown modal has the `is-active` class!
     if (...) {
       ....close();
     } else {
