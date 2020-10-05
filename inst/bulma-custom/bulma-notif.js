78e36401b3d4da09720b561e94435b26ea772a44
DivadNojnarg <dgranjon@ymail.com>
2020-10-05 10:33:42 +0200

    update exercise

--- inst/bulma-custom/bulma-notif.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  Shiny.addCustomMessageHandler('bulma-notif', function(message) {
-    if (message.id === undefined) {
-      Bulma('body'). ...;
-    } else {
-      Bulma('#' + message.id). ...;
-    }
-  });
-});
